f6105e1e1142555c2a028d0cfbe6edc0b4d04d42
minsung Joh <jms5049@naver.com>
2018-09-04 01:44:28 +0900

    Parsing01

--- parseJSON.py
+++ parseJSON.py
@@ -11,3 +11,17 @@ def Keys_and_Values_Dict(dictData) :
 
     for key, value in dictData.items():
         print key, value
+
+def data_to_json(data) :
+    if type(data) is str : 
+        return '"' + data + '"' 
+    elif type(data) is list : 
+	return list_to_json(data, data_to_json)
+    elif type(data) is int or type(data) is float : 
+        return data.__str__() 
+    elif type(data) is dict : 
+        return dict_to_json(data, data_to_json) 
+    else :
+        print("type?€ {}".format(type(data)))
+        return '""'
+
