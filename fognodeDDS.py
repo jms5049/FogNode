8fad570465d758465f0ed8a16fd08d00bb975307
minsung Joh <jms5049@naver.com>
2018-09-06 01:49:51 +0900

    DDS

---
+++ fognodeDDS.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+import socket
+from rule import *
+
+if __name__ == "__main__":
+
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind(('#IP ADDRESS', #PORT))
+    server_socket.listen(1)
+    client_socket, addr = server_socket.accept()
+
+    data = server_socket.recv(1024)
+    print(data)
+
+
+
+    # set_rule(data)
