8390d24ba61cb42ad906aea9850bf6fb8ae7734e
minsung Joh <jms5049@naver.com>
2018-09-08 02:03:35 +0900

    Fog Node - main

--- fognodeMQTT.py
+++ fognodeMQTT.py
@@ -9,3 +9,18 @@ def on_connect(client, userdata, flags, rc):
     print ("Connected with result code " + str(rc))
     client.subscribe("test_fognode")
 
+def on_message(client, userdata, msg):
+    print "Topic: ", msg.topic + '\nMessage: ' + str(msg.payload)
+
+if __name__ == "__main__":
+
+    #mqtt
+    client = mqtt.Client()
+    client.on_connect = on_connect
+    client.on_message = on_message
+    client.connect("iot.eclipse.org", 1883, 60)
+
+    #mqtt    
+    client.loop_forever()
+
+    #dds
